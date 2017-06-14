6cbf33ebb9a09bdb39087a1c89b2aff646158c6c
klone1127 <falcon_sky@yeah.net>
2017-06-15 00:26:26 +0800

    改变音量

--- SoundManager/KPopoverViewController.swift
+++ SoundManager/KPopoverViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import Cocoa
+import AudioToolbox
 
 class KPopoverViewController: NSViewController {
-
+    
+    var sound: NSSound!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        
+    }
+    
+    @IBAction func volumeChangedHandle(_ sender: Any) {
+        print("volume changed")
+        self.changeVolume(0.2)
+    }
+    
+    func changeVolume(_ volumeValue: CGFloat = 0.5) {
+        var defaultOutputDeviceID = AudioDeviceID(0)
+        var defaultOutputDeviceIDSize = UInt32(MemoryLayout.size(ofValue: defaultOutputDeviceID))
+        
+        var getDefaultOutputDevicePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwarePropertyDefaultOutputDevice),
+            mScope: AudioObjectPropertyScope(kAudioObjectPropertyScopeGlobal),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        _ = AudioObjectGetPropertyData(
+            AudioObjectID(kAudioObjectSystemObject),
+            &getDefaultOutputDevicePropertyAddress,
+            0,
+            nil,
+            &defaultOutputDeviceIDSize,
+            &defaultOutputDeviceID)
+        
+        // 设置音量
+        var volume = Float32(volumeValue) // 0.0 ... 1.0
+        let volumeSize = UInt32(MemoryLayout.size(ofValue: volume))
+        
+        var volumePropertyAddress = AudioObjectPropertyAddress(
+            mSelector: AudioObjectPropertySelector(kAudioHardwareServiceDeviceProperty_VirtualMasterVolume),
+            mScope: AudioObjectPropertyScope(kAudioDevicePropertyScopeOutput),
+            mElement: AudioObjectPropertyElement(kAudioObjectPropertyElementMaster))
+        
+        _ = AudioObjectSetPropertyData(
+            defaultOutputDeviceID,
+            &volumePropertyAddress,
+            0,
+            nil,
+            volumeSize,
+            &volume)
     }
     
 }
+
